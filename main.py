40b8a6c0861ed3734a32410da1caff20a2ea155e
daelyn09 <francinesalim@gmail.com>
2026-01-31 23:39:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def blogs():
     n=2
     last=math.ceil(len(postrow)/n)
     page=request.args.get("page")
-    if (not str(page).isnumeric()):
+    if (not str(page).isnumeric()): 
         page=1
     page=int(page)
     j=(page-1)*n
@@ -74,14 +74,14 @@ def blogdetail(slug):
 def settings():
     return render_template("settings.html",param=param)
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def login():
     if request.method=="POST":
         user=request.form["username"]
         password=request.form["password"]
         if user==adminuser and password==adminpassword:
             session["loggedin"]=True
-            return redirect(dashboard)
+            return redirect(url_for("dashboard"))
     return render_template("login.html",param=param)
 
 @app.route("/signup")
@@ -103,10 +103,44 @@ def contact():
 
 @app.route("/admin")
 def dashboard():
-    if "loggedin" in session:
+    if "loggedin" in session: #when admin had logged in to the dashboard, all users' blogs and contacts will appear
         blog=Blog.query.all()
         contact=Contact.query.all()
-        return render_template("admin/admin.html")
+        return render_template("admin/admin.html",blog=blog,contact=contact)
+    else:
+        return redirect(url_for("login")) #if user tries to log in they will be redirected to login page 
+
+@app.route("/editpost/<string:post_id>", methods=["GET","POST"])
+def edit(post_id):
+    if request.method=="POST": #see the blog selected
+        Title=request.form["title"]
+        Subtitle=request.form["subtitle"]
+        Author=request.form["author"]
+        Image=request.form["image"]
+        Location=request.form["location"]
+        Slug=request.form["slug"]
+        Date=datetime.now()
+        Content1=request.form["content1"]
+        Content2=request.form["content2"]
+        if post_id=="new": #if admin wants to uplaod a new post
+            newpost=Blog(title=Title,subtitle=Subtitle,author=Author,image=Image,location=Location,slug=Slug,date=Date,content1=Content1,content2=Content2)
+            db.session.add(newpost)
+            db.session.commit()
+        else:
+            blog=Blog.query.filter_by(post_id=post_id).first() #update the existing blog
+            blog.title=Title
+            blog.subtitle=Subtitle
+            blog.author=Author
+            blog.image=Image
+            blog.location=Location
+            blog.slug=Slug
+            blog.date=Date
+            blog.content1=Content1
+            blog.content2=Content2
+            db.session.commit()
+        return redirect(url_for("dashboard"))
+    blog=Blog.query.filter_by(post_id=post_id).first()
+    return render_template("admin/editpost.html",param=param,blog=blog,post_id=post_id)
 
 if __name__=="__main__":
     with app.app_context():
